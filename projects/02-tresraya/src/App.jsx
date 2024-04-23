a01254d0f869d1d248d99b278a8891fbab3f822b
jmiguelmangas <jmmangas@gmail.com>
2024-04-23 14:56:24 +0200

    Extract to components

--- projects/02-tresraya/src/App.jsx
+++ projects/02-tresraya/src/App.jsx
@@ -1,53 +1,15 @@
 import { useState } from "react"
+import confetti from "canvas-confetti"
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./components/Constants.js"
+import { checkWinnerFrom,checkEndGame } from "./logic/board.js"
 
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-
-
-const Square = ({children, isSelected, updateBoard,index}) =>{
-  const className = `square ${isSelected ? 'is-selected': " "}`
-  const handleClick = () =>{
-    updateBoard(index)
-  }
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
 function App() {
   const [board,setBoard] = useState(Array(9).fill(null))
   const [turn,setTurn] = useState(TURNS.X)
   const [winner,setWinner] = useState(null)
 
-  const checkWinner = (boardToCheck) =>{
-    for (const combo of WINNER_COMBOS){
-      const [a,b,c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ){
-        return boardToCheck[a]
-      }
-    }
-    return null
-  }
+  
   const updateBoard = (index) =>{
     if (board[index] || winner) return
     const newBoard = [...board]
@@ -55,18 +17,16 @@ function App() {
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner){
+      confetti()
       setWinner(newWinner)
     }else if(checkEndGame(newBoard)){
       setWinner(false)
     }
   }
 
-  const checkEndGame = (boardToCheck) =>{
-    return boardToCheck.every((square) => square !== null)
-  }
-
+  
   const resetGame = () =>{
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
