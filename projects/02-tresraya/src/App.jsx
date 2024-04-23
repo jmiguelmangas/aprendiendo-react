56baa70f94a424bc9e50094c7ad30a747afdb2e7
jmiguelmangas <jmmangas@gmail.com>
2024-04-23 15:58:17 +0200

    Savegame to local storage

--- projects/02-tresraya/src/App.jsx
+++ projects/02-tresraya/src/App.jsx
@@ -3,20 +3,32 @@ import confetti from "canvas-confetti"
 import { Square } from "./components/Square.jsx"
 import { TURNS } from "./components/Constants.js"
 import { checkWinnerFrom,checkEndGame } from "./logic/board.js"
-
+import { Winner } from "./Winner.jsx"
 function App() {
-  const [board,setBoard] = useState(Array(9).fill(null))
-  const [turn,setTurn] = useState(TURNS.X)
+  const [board,setBoard] = useState(()=>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+})
+  const [turn,setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem("turn")
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner,setWinner] = useState(null)
 
   
   const updateBoard = (index) =>{
-    if (board[index] || winner) return
+    if (board[index] || winner) return //miramos si ya hay algo en esa posicion, o si ya ha ganado
+    //actualizamos el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+    //cambiamos el turno al otro jugador
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //Guardamos en Almacenamiento Local
+    window.localStorage.setItem('board',JSON.stringify(newBoard))
+    window.localStorage.setItem('turn',newTurn)
+    //Revisamos sin hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner){
       confetti()
@@ -26,11 +38,13 @@ function App() {
     }
   }
 
-  
+  //resetear el juego
   const resetGame = () =>{
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
   
 
@@ -57,27 +71,7 @@ function App() {
         <Square isSelected={turn=== TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn=== TURNS.O}>{TURNS.O}</Square>
       </section>
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                  ? 'Empate'
-                  : "Ganó:"
-                }
-              </h2>
-              <header className="win">
-                {winner && <Square>{winner}</Square>}
-              </header>
-              <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+     <Winner resetGame={resetGame} winner={winner}/>
     </main>
   )
 }
